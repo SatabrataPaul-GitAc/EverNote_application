f3af4d7abbbf264a8fc12aa0357445f163d084ff
SatabrataPaul-GitAc <69891965+SatabrataPaul-GitAc@users.noreply.github.com>
2021-08-03 22:28:08 +0530

    Update utilities.py

--- evernote_app/post_blog_app/utilities.py
+++ evernote_app/post_blog_app/utilities.py
@@ -9,7 +9,7 @@ def save_profile_pic(form_picture):
     random_hex = secrets.token_hex(8)
     _,f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(web_app.root_path,"static\profile_pic",picture_fn)
+    picture_path = os.path.join(web_app.root_path,"static/profile_pic",picture_fn)
     output_size = (150,150)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
